281516e8ea81b294599759bb5de4b0582cb84028
Simão Gato <simao.valedegato@gmail.com>
2025-01-04 10:03:06 +0000

    fix: correct fugitive group

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-		local Sig_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+		local Sig_Fugitive = vim.api.nvim_create_augroup("Sig_Fugitive", {})
 
 		local autocmd = vim.api.nvim_create_autocmd
 		autocmd("BufWinEnter", {

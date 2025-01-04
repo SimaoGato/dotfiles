b773970961d3c9c64e3904524029ffd6100bd684
Simão Gato <simao.valedegato@gmail.com>
2025-01-04 10:10:33 +0000

    do: add undotree plugin

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -41,4 +41,11 @@ return {
 			require("colorizer").setup()
 		end,
 	},
+	{
+		"mbbill/undotree",
+
+		config = function()
+			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+		end,
+	},
 }
